c7b3863843c9689b00f387d59f3bbc0db734fb9b
Antoni Wojciech Skrobek <01159454@pw.edu.pl>
2024-11-06 19:51:42 +0100

    Book detail screen ready

--- app/src/main/java/com/example/bachelorv1/ui/book_details/BookDetailsScreen.kt
+++ app/src/main/java/com/example/bachelorv1/ui/book_details/BookDetailsScreen.kt
@@ -13,8 +13,6 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyRow
 import androidx.compose.material.icons.Icons
